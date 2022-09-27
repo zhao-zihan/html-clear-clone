0f89a6778b17a6aae0333dff4c22f48352bbc7fd
Zihan Zhao <zihan0932@gmail.com>
2022-09-27 00:21:38 -0700

    Delegate render function to child class

--- scripts/item.js
+++ scripts/item.js
@@ -5,9 +5,6 @@ class Item {
 
   constructor(data) {
     this.data = data;
-    this.htmlEl = [];
-    this.style = this.htmlEl[0].style;
-    this.slider = this.htmlEl.querySelectorAll(".slider");
-    this.sliderStyle = this.slider[0].style;
+    // this._render();
   }
 }
