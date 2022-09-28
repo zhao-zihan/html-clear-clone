24094738b191ea4bde05c10c2ae6ee25992319a6
Zihan Zhao <zihan0932@gmail.com>
2022-09-28 13:35:02 -0700

    Create addImage function to add cross and check img to el, simplify constructor code

--- scripts/item.js
+++ scripts/item.js
@@ -11,12 +11,40 @@ class Item {
     }
     this._render();
     // console.log("item el: " + this.el.innerHTML);
-    console.log("check el: " + this.el);
+    console.log("check el: " + elementsToHTML(this.el));
+    this._selectStyle();
+    // console.log("check first element: " + this.slider.innerHTML);
+
+    this._addImage();
+
+    this.title = this.el.querySelector(".title");
+    this.field = this.el.querySelector(".field");
+    console.log("check this.field " + elementsToHTML(this.field));
+  }
+
+  _selectStyle() {
     this.style = this.el.style;
     this.slider = this.el.querySelector(".slider");
     this.sliderStyle = this.slider.style;
-    this.slider.style.backgroundColor = "gb(23, 128, 247)";
-    // console.log("check first element: " + this.slider.innerHTML);
+  }
+
+  _addImage() {
+    this.check = htmlToElements(
+      '<img class="check drag" src="images/check.png">'
+    );
+    this.cross = htmlToElements(
+      '<img class="cross drag" src="images/cross.png">'
+    );
+    this.el.appendChild(this.check);
+    this.el.appendChild(this.cross);
+
+    this.checkStyle = this.check.style;
+    this.crossStyle = this.cross.style;
+
+    this.checkX = 0;
+    this.crossX = 0;
+    this.checkO = 0;
+    this.crossO = 0;
   }
 
   _updateCount() {
