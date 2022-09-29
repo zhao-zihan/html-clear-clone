3c11d17b8d4ba12bb1917cb05f1a3ce6972ee3aa
Zihan Zhao <zihan0932@gmail.com>
2022-09-28 19:57:02 -0700

    Add moving methods

--- scripts/item.js
+++ scripts/item.js
@@ -1,8 +1,6 @@
 "use strict";
 
 class Item {
-  itemHeight = 62;
-
   constructor(data, listItem) {
     this.data = data;
     // this._render();
@@ -17,13 +15,14 @@ class Item {
 
     this._addImage();
 
-    this.title = this.el.querySelector(".title");
-    this.field = this.el.querySelector(".field");
-    console.log("check this.field " + elementsToHTML(this.field));
+    this._listenField();
+
+    // console.log("check this.field " + elementsToHTML(this.field));
   }
 
   _selectStyle() {
     this.style = this.el.style;
+    console.log("this.style: " + JSON.stringify(this.style));
     this.slider = this.el.querySelector(".slider");
     this.sliderStyle = this.slider.style;
   }
@@ -47,6 +46,12 @@ class Item {
     this.crossO = 0;
   }
 
+  _listenField() {
+    this.title = this.el.querySelector(".title");
+    this.field = this.el.querySelector(".field");
+    // this.field.addEventListener("blur", this._onEditEnd.bind(this));
+  }
+
   _updateCount() {
     this.count = 0;
 
@@ -55,5 +60,63 @@ class Item {
     this.data.items.forEach((item) => {
       if (!item.done) this.count++;
     });
+  }
+
+  _moveY(y) {
+    this.y = y;
+    this.style[transformProperty] = `translate3d(0px, ${y}px, 0px)`;
+  }
+
+  _moveX(x) {
+    this.x = x;
+    this.sliderStyle[transformProperty] = `translate3d(${x}px, 0px, 0px)`;
+  }
+
+  _moveCheck(x) {
+    this.checkX = x;
+    this.checkStyle[transformProperty] = `translate3d(${x}px, 0px, 0px)`;
+  }
+
+  _moveCross(x) {
+    this.crossX = x;
+    this.crossStyle[transformProperty] = `translate3d(${x}px, 0px, 0px)`;
+  }
+
+  _updatePosition(top) {
+    if (top) {
+      this.el.classList.add(".top");
+    }
+
+    this._moveY(this.data.order * ITEM_HEIGHT);
+
+    if (top) {
+      this._onTransitionEnd(function (t) {
+        t.el.classList.remove(".top");
+      });
+    }
+  }
+
+  _onTransitionEnd(callback, noStrict) {
+    const t = this;
+    t.el.addEventListener(transitionEndEvent, function (e) {
+      if (e.target !== this && !noStrict) return;
+      t.el.removeEventListener(transitionEndEvent);
+      callback(t);
+    });
+  }
+
+  _onEditStart(noRemember) {
+    app.isEditing = true;
+
+    this.title.display = "none";
+    this.field.display = "block";
+    this.field.focus();
+    this.el.classList.add("edit");
+
+    // this.collection._onEditStart(this.data.order, noRemember);
+  }
+
+  _onEditDone() {
+    const t = this;
   }
 }
