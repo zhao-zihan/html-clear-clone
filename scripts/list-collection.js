da074a796ee2ea4cf2067720cf84393452168c62
Zihan Zhao <zihan0932@gmail.com>
2022-10-02 01:00:40 -0700

    Fix bug in open function, correct animation positions

--- scripts/list-collection.js
+++ scripts/list-collection.js
@@ -2,7 +2,6 @@
 
 class ListCollection extends Collection {
   constructor(data) {
-    console.log("arguments in lc constructor" + data);
     super(data);
     this.stateType = states.LIST_COLLECTION_VIEW;
     this.itemType = function (data) {
@@ -10,7 +9,8 @@ class ListCollection extends Collection {
     };
     this.itemTypeText = "List";
 
-    this._render();
+    // this._render();
+
     this._populateItems();
     this._updateColor();
   }
@@ -34,6 +34,71 @@ class ListCollection extends Collection {
   _load() {
     this.initiated = true;
     // itemContainer.insertAdjacentHTML("afterend", this.el.innerHTML);
-    itemContainer.innerHTML = this.el.innerHTML;
+    // console.log("this.el in load: " + elementsToHTML(this.el));
+    // itemContainer.innerHTML = elementsToHTML(this.el);
+    itemContainer.appendChild(this.el);
+  }
+
+  _open(at) {
+    this.openedAt = at;
+
+    let t = this;
+    let ty;
+
+    // let i = t.items.length;
+    // let item;
+    // let ty;
+
+    // while (i--) {
+    //   item = t.items[i];
+    //   if (item.data.order <= at) {
+    //     ty = (item.data.order - at) * ITEM_HEIGHT - t.y;
+    //   } else {
+    //     ty = window.innerHeight + (item.data.order - at) * ITEM_HEIGHT - t.y;
+    //   }
+    //   item.moveY(ty);
+    // }
+
+    t.items
+      .slice()
+      .reverse()
+      .forEach((item) => {
+        ty =
+          item.data.order <= at
+            ? (item.data.order - at - 1) * ITEM_HEIGHT
+            : (t.data.items[at].items.length + (item.data.order - at - 1)) *
+              ITEM_HEIGHT;
+        item._moveY(ty);
+      });
+
+    t.items[0]._onTransitionEnd(function () {
+      t._positionForPullDown();
+    });
+  }
+
+  _positionForPullDown() {
+    const t = this;
+    t.el.style.display = "none";
+
+    setTimeout(function () {
+      t._updatePosition();
+      t._moveY(-t.height - ITEM_HEIGHT);
+      t.el.classList.add("drag");
+      t.el.display = "block";
+    }, 0);
+  }
+
+  _onDragMove() {
+    super._onDragMove.apply(this, arguments);
+
+    const ltc = app.lastTodoCollection;
+
+    if (this.y <= this.upperBound) {
+      if (!this.longPullingUp) {
+        this.longPullingUp = true;
+        ltc.el.classList.add("drag");
+        ltc.topSwitch.display = "block";
+      }
+    }
   }
 }
