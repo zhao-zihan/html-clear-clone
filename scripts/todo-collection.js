f0c263ed7b7cb2ad7a54973d151f3ff1b5c94c9c
Zihan Zhao <zihan0932@gmail.com>
2022-10-02 01:02:13 -0700

    Complete constructor and load function

--- scripts/todo-collection.js
+++ scripts/todo-collection.js
@@ -2,23 +2,25 @@
 
 class TodoCollection extends Collection {
   constructor(data, listItem) {
-    super(arguments);
+    super(data);
+    console.log("super called");
     this.stateType = states.TODO_COLLECTION_VIEW;
-    this.itemType = function () {
-      return new TodoItem(arguments);
+    this.itemType = function (data) {
+      return new TodoItem(data);
     };
     this.itemTypeText = "Item";
     this.listItem = listItem;
+    this._populateItems();
   }
 
   _render() {
     this.el = htmlToElements(`
     <div class="collection">
         <div class="top-switch">
-            <img class="arrow" src="img/arrow.png"> <span class="text">Switch To Lists</span>
+            <img class="arrow" src="images/arrow.png"> <span class="text">Switch To Lists</span>
         </div>
         <div class="bottom-switch">
-            <div class="drawer"><img class="arrow-small" src="img/arrow-small.png"></div>
+            <div class="drawer"><img class="arrow-small" src="images/arrow-small.png"></div>
             <span class="text">Pull to Clear</span>
         </div>
         <div class="item dummy-item top">
@@ -30,6 +32,15 @@ class TodoCollection extends Collection {
     `);
 
     this.style = this.el.style;
+
+    this.topSwitch = this.el.querySelector(".top-switch");
+    this.topArrow = this.topSwitch.querySelector(".arrow");
+    this.topText = this.topArrow.querySelector(".text");
+
+    this.bottomSwitch = this.el.querySelector(".bottom-switch");
+    this.drawer = this.bottomSwitch.querySelector(".drawer");
+    this.smallArrowStyle =
+      this.bottomSwitch.querySelector(".arrow-small").style;
   }
 
   _load(at, noAnimation) {
@@ -39,6 +50,29 @@ class TodoCollection extends Collection {
 
     t._updateColor();
 
-    itemContainer.insertAdjacentHTML("afterend", this.el);
+    if (noAnimation) {
+      this._updatePosition();
+      itemContainer.prepend(this.el);
+    } else {
+      if (t.initiated) {
+        t.el.remove();
+      }
+      t._moveY(at * ITEM_HEIGHT + app.listCollection.y);
+      t.items.forEach((item) => item._moveY(0));
+
+      t.el.classList.remove("drag");
+      itemContainer.prepend(this.el);
+
+      setTimeout(function () {
+        t._moveY(0);
+        t._updatePosition();
+      }, 0);
+    }
+  }
+
+  _updateCount() {
+    this.listItem.count = this.count;
+    this.listItem._updateCount();
+    this.hasDoneItems = this.items.length > this.count;
   }
 }
